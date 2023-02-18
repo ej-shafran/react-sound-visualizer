d2a67045ddbcd8b473b728d4ada44cf921849b43
ej shafran <116496520+ej-shafran@users.noreply.github.com>
2023-02-18 17:15:43 +0200

    Add JSDoc (useVisualizer)

--- src/useVisualizer.tsx
+++ src/useVisualizer.tsx
@@ -14,13 +14,30 @@ type ContinuousOptions = {
   mode?: "continuous";
 } & DrawContinuousOptions;
 
+/**
+ * The options passed to `useVisualizer`.
+ * Also part of the props for `Visualizer`, and the options for `visualizerWrapper`.
+ **/
 export type UseVisualizerOptions = CurrentOptions | ContinuousOptions;
 
+type UndefinedValues<T> = {
+  [K in keyof T]: undefined;
+}
+
+/**
+ * Hook that wraps the `visualizer` functions from `sound-visualizer` with a `useMemo`.
+ * 
+ * @param audio the audio to visualize
+ * @param canvas the canvas to draw on
+ * @param options additional options for the `draw` functions.
+ * 
+ * @returns either the `VisualizerFunctions` from `sound-visualizer` or an empty object, depending on if `audio` or `canvas` are `null` or not.
+ **/
 export function useVisualizer(
   audio: MediaStream | null,
   canvas: HTMLCanvasElement | null,
   options: UseVisualizerOptions
-): Partial<VisualizerFunctions> {
+): VisualizerFunctions | UndefinedValues<VisualizerFunctions> {
   return useMemo(() => {
     return visualizerWrapper(audio, canvas, options);
   }, [canvas, audio]);
